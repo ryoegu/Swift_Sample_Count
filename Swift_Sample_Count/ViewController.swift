37730387136fc2a20ef09da672eb7f9fa3a74b74
Ryo Eguchi <ryo.egu@gmail.com>
2014-12-31 01:32:30 +0900

    Initial Commit

--- Swift_Sample_Count/ViewController.swift
+++ Swift_Sample_Count/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //変数の宣言
+    var number = 0
+    //関連付け
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(sender: AnyObject) {
+        number = number + 1 //number++でも可
+        updateLabel() //updateLabelを呼び出す
+    }
+    
+    //呼び出されたもの
+    func updateLabel(){
+        if number>=20{
+            label.textColor = UIColor.redColor()
+        }else if number>=10{
+            label.textColor = UIColor.blueColor()
+        }else {
+            label.textColor=UIColor.blackColor()
+        }
+        
+        label.text = "\(number)" //labelに表示
+    }
 
 }
 
