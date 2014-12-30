040ed9d663831fd627023c4a776d012731f74a7a
Ryo Eguchi <ryo.egu@gmail.com>
2014-12-31 01:19:12 +0900

    Initial Commit

---
+++ Swift_Sample_Count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Sample_Count
+//
+//  Created by Ryo Eguchi on 2014/12/31.
+//  Copyright (c) 2014年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
